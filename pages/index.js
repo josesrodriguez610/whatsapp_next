6db6bde2a83933527f836c593c79fa7586b420b6
josesrodriguez610 <josesrodriguez610@gmail.com>
2021-03-30 22:21:29 -0500

    (add) Sidebar Nav and search bar

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import Sidebar from "../components/Sidebar";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>What's up 2.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Let's build What's up 2.0</h1>
+      <Sidebar />
     </div>
   );
 }
